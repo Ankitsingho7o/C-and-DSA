7917ddac2a3112dbd6d6839706af602414e838aa
Ankit <12kankitsingh@gmail.com>
2022-10-14 23:55:59 +0530

    Prime number and for loop

---
+++ .vscode/prime.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int flag=0;
+    for (int i = 2; i < n; i++)
+    {
+        if(n%i==0){
+            cout<<"not prime ";
+            flag=1;
+            break;
+        }
+
+    }
+    if(flag==0){
+        cout<<"prime";
+    }
+
+}
--- forunderstanding.cpp
+++ forunderstanding.cpp
@@ -12,14 +12,31 @@ int main()
 
     while (i <= n)
     {
-        int j = i;
+        int j = 1;
 
-        while (j <= n)
+        while (j <= n - i + 1)
         {
 
-            cout << i;
+            cout << j;
             j = j + 1;
         }
+
+        cout << endl;
+        i += 1;
+    }
+    i = 1;
+    while (i <= n)
+    {
+
+        int k = 1;
+
+        while (k <= i)
+        {
+
+            cout << k;
+            k = k + 1;
+        }
+
         cout << endl;
         i += 1;
     }
