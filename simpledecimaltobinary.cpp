72427a68089f4fca67b31ede2e7073951b0044f7
Ankit <12kankitsingh@gmail.com>
2022-10-17 22:07:15 +0530

    Reverse number

---
+++ simpledecimaltobinary.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int answer = 0;
+    // int bit;
+    int i=0;
+
+    while (n != 0)
+    {
+       int bit = n & 1;
+
+        answer =( bit * pow(10, i) )+ answer;
+
+        n = n >> 1;
+        i = i + 1;
+    }
+    cout << answer;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,20 @@
-   for(int i=0; i<b;i++){
-       answer = answer*a;
+#include <iostream>
+#include <math.h>
 
-    }
+using namespace std;
+
+int main()
+{
+int a;
+cin>>a;
+int answer=0;
+
+while(a!=0){
+int digit = a%10;
+cout<<"in";
+ answer = (answer*10)+digit;
+  a= a/10;
+}
+
+cout<<answer;
+}
